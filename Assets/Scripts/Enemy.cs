791f3ab77582fee1154acd8fec43eed68fa30224
ArtemVetik <av.unitygames@gmail.com>
2020-03-21 16:19:15 +0300

    Changed Enemy script

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float _movementSpeed;
+    [SerializeField] private float _spawnRadius;
+
+    private Vector3 _targetPosition;
+
+    private void Start()
+    {
+        _targetPosition = Random.insideUnitCircle * _spawnRadius;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _targetPosition, _movementSpeed * Time.deltaTime);
+        if (transform.position == _targetPosition)
+            _targetPosition = Random.insideUnitCircle * _spawnRadius;
+    }
+}
